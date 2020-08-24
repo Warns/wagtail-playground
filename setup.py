2fadfb9d9eef88adf6edc61676edc881230d5ab4
Mert Alnuaimi <snickns@gmail.com>
2020-08-24 18:03:21 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='mysite',
+    install_requires=[
+        'Django>=3.1,<3.2',
+        'wagtail>=2.10,<2.11',
+    ],
+    packages=[
+        'home',
+        'mysite',
+        'search',
+        'home.migrations',
+        'mysite.settings',
+    ],
+    py_modules=[
+        'manage',
+    ],
+)
