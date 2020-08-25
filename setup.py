a3cc4657ce71c054a6f2a8fa0fa3eed9c9512c11
Mert Alnuaimi <snickns@gmail.com>
2020-08-25 08:43:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@
 from setuptools import setup
 
 setup(
-    name="mysite",
-    install_requires=["Django>=3.1,<3.2", "wagtail>=2.11,<2.12",],
+    name="testingen",
+    install_requires=["Django>=3.1,<3.2", "wagtail>=2.12,<2.13",],
     packages=["home", "mysite", "search", "home.migrations", "mysite.settings",],
     py_modules=["manage",],
 )
